0c94b01f565f7d38f2e4bb35811b1c1e7eab6069
Edgar <eadrian2018@gmail.com>
2026-05-27 12:41:12 -0700

    restore complete patient detail routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import {
+
   Routes,
+
   Route,
+
   Navigate,
+
 } from "react-router-dom";
 
 import { useState } from "react";
@@ -12,9 +16,9 @@ import Login from "./components/Login";
 
 import Dashboard from "./pages/dashboardNEW";
 
-import Pacientes from "./pages/pacientesNEW";
-
 import AgendaCalendar from "./pages/agendaCalendar";
+
+import Pacientes from "./pages/pacientesNEW";
 
 import PacienteDetalle from "./pages/pacientedetalle.tsx";
 
@@ -34,35 +38,55 @@ export default function App() {
     <Routes>
 
       <Route
+
         path="/"
+
         element={<Layout />}
+
       >
 
         <Route
+
           index
+
           element={
+
             <Navigate to="/dashboard" />
+
           }
+
         />
 
         <Route
+
           path="/dashboard"
+
           element={<Dashboard />}
+
         />
 
         <Route
+
           path="/agenda"
+
           element={<AgendaCalendar />}
+
         />
 
         <Route
+
           path="/pacientes"
+
           element={<Pacientes />}
+
         />
 
         <Route
+
           path="/paciente/:id"
+
           element={<PacienteDetalle />}
+
         />
 
       </Route>
